365388fc14c477473ffab978083c7f3144973557
LUCIA FARDELLA <lfardella@uc.cl>
2025-01-09 16:27:03 -0300

    cambio gpu/cpu

--- scamp_1224/run.sh
+++ scamp_1224/run.sh
@@ -5,12 +5,12 @@
 # Archivo de salida
 #SBATCH --output=salida.txt
 # Partición CPUS
-#SBATCH --partition=512x1024
+#SBATCH --partition=full
 # Partición GPUS
 ##SBATCH --partition=gpus
 # Solicitud de cpus
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=8
 # Solicitud de GPUS
 ##SBATCH --gres=gpu:1
 #SBATCH --mail-user=lfardella@uc.cl
