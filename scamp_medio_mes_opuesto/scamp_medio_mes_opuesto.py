bcb428d61d5e59964e00e66fe39aac3aca25c4ed
LUCIA FARDELLA <lfardella@uc.cl>
2025-01-23 16:00:23 -0300

    cambio en archivo de datos

--- scamp_medio_mes_opuesto/scamp_medio_mes_opuesto.py
+++ scamp_medio_mes_opuesto/scamp_medio_mes_opuesto.py
@@ -2,7 +2,7 @@ from obspy import read
 import pyscamp as mp
 import numpy as np
 
-Data   = read('C1.MT12.HH*.2017.182-212.0merge.mseed')
+Data   = read('C1.MT12.HH*.2017.197-212.0merge.mseed')
 SR     = Data[0].stats.sampling_rate 
 sublen = int(11*SR)
 
